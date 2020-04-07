d4ab19e78d46d3d12f66c619f82b9fe772219bea
Doenator <David@dieterhenkel.de>
2020-04-07 12:33:59 +0200

    Test

--- app.js
+++ app.js
@@ -6,9 +6,10 @@ const port = 8080;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World!\n');
+    res.end(' Hello World!\n');
 });
 
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
+
