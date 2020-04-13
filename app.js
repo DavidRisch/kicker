b4454c039a7e89f3d699a9d96469546d203fc00b
David Risch <acc_mid@rischnet.de>
2020-04-13 15:19:59 +0200

    Added database connection

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-const port = 8080
+const dotenv = require('dotenv')
+const path = require('path')
+
+if (dotenv.config({ path: path.resolve(__dirname, 'config', 'config.env') }).error) {
+  console.warn('config/config.env not found, using dummy config at config/config.env.template')
+  dotenv.config({ path: path.resolve(__dirname, 'config', 'config.env.template') })
+}
 
 const express = require('express')
 const app = express()
 
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: true }))
-
-app.set('view engine', 'html')
-app.set('views', __dirname + '/html')
 
 app.use('/css', express.static('css'))
 app.use('/js', express.static('js'))
@@ -20,6 +23,9 @@ app.get('/', function (req, res) {
 require('./page/page')(app)
 require('./api/api')(app)
 
-app.listen(port, function () {
-  console.log('Listening on port ' + port + '!')
+// enable to test database connection:
+// console.log(require('./src/database').query('SELECT :abc,:def', { abc: 123, def: 'test' }))
+
+app.listen(process.env.HTTP_PORT, function () {
+  console.log('Listening on port ' + process.env.HTTP_PORT + '!')
 })
