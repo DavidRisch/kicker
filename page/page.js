7232bb9060eb67bab3c37e9817df6837490a767b
Lixfel <agga-games@gmx.de>
2020-04-14 11:53:10 +0200

    Implementing a first login method switcher

--- page/page.js
+++ page/page.js
@@ -5,6 +5,9 @@ function init (app) {
   app.get('/example', function (req, res) {
     require('./example').page(req, res)
   })
+  app.get('/choose_login_method', function (req, res) {
+    require('./choose_login_method').page(req, res)
+  })
 
 // ^^^ Insert new pages here ^^^
 
