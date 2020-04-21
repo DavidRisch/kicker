badc4f4f40c7f55cf3179f61187c2d8a82a6c59e
Doenator <David@dieterhenkel.de>
2020-04-21 12:11:15 +0200

    Renamed inputvalidator.html to inputvalidator_example.html because it is an example

--- page/inputvalidator.js
+++ page/inputvalidator.js
@@ -5,7 +5,7 @@ module.exports = {
 function page (req, res) {
     console.log('making validator page...')
     var fs = require('fs')
-    fs.readFile('html/inputvalidator.html', 'utf8', function (err, html) {
+    fs.readFile('html/inputvalidator_example.html', 'utf8', function (err, html) {
         if (err) throw err
         
         res.end(html)
