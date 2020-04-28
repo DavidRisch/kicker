b43e615c2fdc7e1c8445a1806706ced8b453c4ae
Lixfel <agga-games@gmx.de>
2020-04-28 12:04:44 +0200

    Implementing html header options

--- src/html_creator.js
+++ src/html_creator.js
@@ -5,24 +5,28 @@ module.exports =
     create_html: createHtml
   }
 
-function htmlHeader (title) {
-  return `<!DOCTYPE html>
-  <html lang="en">
+function htmlHeader (title, js = [], additional = '') {
+  result = `<!DOCTYPE html>
+    <html lang="de">
     <head>
     <meta charset="UTF-8">
     <title>${title}</title>
+    <link type="text/css" rel="stylesheet" href="css/style.css"/>`
 
-    <link type="text/css" rel="stylesheet" href="css/style.css"/>
+  js.forEach(function (scriptfile) {
+    result += `<script type="text/javascript" src="js/${scriptfile}.js"></script>`
+  })
 
-    <script type="text/javascript" src="js/example.js"></script>
-    </head>
-    <body>`
+  result += additional
+
+  result += `</head><body>`
+  return result
 }
 
 function htmlFooter () {
   return '</body></html>'
 }
 
-function createHtml (html, title) {
-  return htmlHeader(title) + html + htmlFooter()
+function createHtml (html, title, js = []) {
+  return htmlHeader(title, js) + html + htmlFooter()
 }
