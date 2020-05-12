b2a405004d6eb7df2cc80b941b6a4dbaa5b8ae0c
David Risch <acc_mid@rischnet.de>
2020-05-12 13:32:30 +0200

    Fixed create_html() usage in enter_game

--- page/enter_game.js
+++ page/enter_game.js
@@ -2,9 +2,12 @@ function page (req, res) {
   require('fs').readFile('html/enter_game.html', 'utf8', function (err, html) {
     if (err) throw err
 
-    res.end(require('../src/html_creator').create_html(html, 'Neues Spiel',
-      ['enter_game'],
-      ['styles_general']))
+    res.end(require('../src/html_creator').create_html(html, {
+      title: 'Neues Spiel',
+      js: ['enter_game'],
+      css: ['styles_general'],
+      nav: true
+    }))
   })
 }
 
