650698cded5552fc768f38c8f9c0311ad0d13fa5
David Risch <acc_mid@rischnet.de>
2020-05-17 23:18:32 +0200

    Applied formatter

--- page/achievements.js
+++ page/achievements.js
@@ -1,16 +1,15 @@
 function page (req, res) {
-    require('fs').readFile('html/achievements.html', 'utf8', function (err, html) {
-      if (err) throw err
-  
-      res.end(require('../src/html_creator').create_html(html, {
-        title: 'Erfolge',
-        css: ['styles_general', 'achievements'],
-        nav: true
-      }))
-    })
-  }
-  
-  module.exports = {
-    page: page
-  }
-  
+  require('fs').readFile('html/achievements.html', 'utf8', function (err, html) {
+    if (err) throw err
+
+    res.end(require('../src/html_creator').create_html(html, {
+      title: 'Erfolge',
+      css: ['styles_general', 'achievements'],
+      nav: true
+    }))
+  })
+}
+
+module.exports = {
+  page: page
+}
