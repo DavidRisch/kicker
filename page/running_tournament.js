25805e4afc4035fda188746dbd931153566bd003
David Risch <acc_mid@rischnet.de>
2020-05-17 18:31:42 +0200

    Applied formatter

--- page/running_tournament.js
+++ page/running_tournament.js
@@ -1,16 +1,15 @@
 function page (req, res) {
-    require('fs').readFile('html/running_tournament.html', 'utf8', function (err, html) {
-      if (err) throw err
-  
-      res.end(require('../src/html_creator').create_html(html, {
-        title: 'Turniername',
-        css: ['styles_general', 'row'],
-        nav: true
-      }))
-    })
-  }
-  
-  module.exports = {
-    page: page
-  }
-  
+  require('fs').readFile('html/running_tournament.html', 'utf8', function (err, html) {
+    if (err) throw err
+
+    res.end(require('../src/html_creator').create_html(html, {
+      title: 'Turniername',
+      css: ['styles_general', 'row'],
+      nav: true
+    }))
+  })
+}
+
+module.exports = {
+  page: page
+}
