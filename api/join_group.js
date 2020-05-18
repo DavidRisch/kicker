43906ed1968a24f3535130bd770ddd4f2930c0fe
Sebastian <wallatsebastian@gmail.com>
2020-05-18 15:37:28 +0200

    Refractored link generation

--- api/join_group.js
+++ api/join_group.js
@@ -22,9 +22,12 @@ function joinGroup (token) {
 function createInvite (req, res) {
   // debug function to get links for testing
   // change below params
-  const userId = 5
-  const groupId = 1
   const host = req.protocol + '://' + req.get('host')
+  const url = new URL(host + req.url)
+  const user = url.searchParams.get('user')
+  const group = url.searchParams.get('group')
+  const userId = require('../src/db/user').by_name(user).id
+  const groupId = require('../src/db/group').by_name(group).id
   const invite = require('../src/db/group_invitations').create(groupId, userId).createLink(host)
   res.writeHead(200, { 'Content-Type': 'text/json' })
   return { link: invite }
