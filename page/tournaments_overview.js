588310600c75b60027752b832bad3b6d22c2cb4a
LukasR456 <43873482+LukasR456@users.noreply.github.com>
2020-05-19 14:18:22 +0200

    Added tournament overview page.

---
+++ page/tournaments_overview.js
@@ -0,0 +1,16 @@
+function page (req, res) {
+    require('fs').readFile('html/tournaments_overview.html', 'utf8', function (err, html) {
+      if (err) throw err
+  
+      res.end(require('../src/html_creator').create_html(html, {
+        title: 'Turniere',
+        css: ['styles_general'],
+        nav: true
+      }))
+    })
+  }
+  
+  module.exports = {
+    page: page
+  }
+  
