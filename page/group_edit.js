76c97591aed73f2a5fcc41f7d591d61ac71aad27
Dennis220999 <43820947+Dennis220999@users.noreply.github.com>
2020-05-19 14:52:21 +0200

    complete group_creation, start of edit

---
+++ page/group_edit.js
@@ -0,0 +1,17 @@
+function page (req, res) {
+    require('fs').readFile('html/group_edit.html', 'utf8', function (err, html) {
+        if (err) throw err
+
+        res.end(require('../src/html_creator').create_html(html, {
+            title: 'Gruppen Bearbeitung',
+            js: ['jquery', 'jquery-ui', 'tokenize2', 'dropzone', 'group_edit'],
+            css: ['bootstrap','tokenize2','styles_general', 'jquery-ui', 'dropzone'],
+            nav: true
+        }))
+    })
+}
+
+module.exports = {
+    page: page
+}
+
