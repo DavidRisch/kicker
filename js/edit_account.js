820cfe807d53d2117ef5892b34b97412d2309ad0
David Risch <acc_mid@rischnet.de>
2020-05-20 12:08:03 +0200

    Improved js/edit_account

--- js/edit_account.js
+++ js/edit_account.js
@@ -1,4 +1,6 @@
-async function Submit () {
+/* global apiPost */
+
+async function Submit () { // eslint-disable-line no-unused-vars
   const data = {
     action: '',
     name: document.getElementById('userNameInput').value,
@@ -21,17 +23,22 @@ async function Submit () {
   }
 }
 
-async function LoadEntries () {
+async function LoadEntries () { // eslint-disable-line no-unused-vars
   // reset entries with user data
   const data = {
     action: 'getUserData'
   }
   const res = await apiPost(data)
+  if (res.success !== true) {
+    window.location.replace('/login')
+  }
   document.getElementById('mailInput').value = res.email
   document.getElementById('userNameInput').value = res.name
   document.getElementById('phoneNumberInput').value = res.telephone
   // delete password entry (placeholder indicates a password is set)
   document.getElementById('passwordInput').value = ''
+
+  document.getElementById('errorLabel').innerHTML = ''
 }
 
 async function ValidateUserInput (data) {
@@ -50,7 +57,7 @@ async function ValidateUserInput (data) {
     }
   }
   if (!res.validName) {
-    label.innerHTML = 'Ungültiger Nutzername (mind. 5 Zeichen)'
+    label.innerHTML = 'Ungültiger Nutzername (mind. 5 Zeichen aus A-Z, a-z, 0-9, _, -)'
     return false
   }
 
--- page/account.js
+++ page/account.js
@@ -4,7 +4,6 @@ module.exports = {
 
 function page (req, res) {
   require('../src/account_util').require_logged_in_user(req, res)
-  console.log('making account page...')
 
   require('fs').readFile('html/account.html', 'utf8', function (err, html) {
     if (err) throw err
