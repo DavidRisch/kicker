8fe1ccc1dd42a6885a3cd7a92bed088bdb560648
AnneRei <anne.reichert97@gmail.com>
2020-05-20 09:59:50 +0200

    handled user is null

--- api/get_user_data.js
+++ api/get_user_data.js
@@ -1,8 +1,14 @@
 function process (req) {
     const user = require('../src/account_util').get_current_user(req)
-
+    if (user === null) {
+        return {
+            success: false,
+            errorReason: 'user does not exist'
+        }
+    }
     let response = ''
     response = {
+        success: true,
         name: user.name,
         email: user.email,
         telephone: user.telephone
