023448e036a849461dff1ee5269fb48eb9669090
David Risch <acc_mid@rischnet.de>
2020-05-26 13:38:35 +0200

    Fixed bugs in statistics

--- js/example_statistics.js
+++ js/example_statistics.js
@@ -1,56 +1,62 @@
-var winLoosePieCtx = document.getElementById("winLoosePie").getContext("2d");
-var winLoosePie = new Chart(winLoosePieCtx, {
-  type: "pie",
-  data: {
-    labels: ["Wins", "Looses"],
-    datasets: [
-      {
-        data: [90, 20],
-        borderWidth: 0,
-        backgroundColor: ["#31bebeff", "#12141eff"],
-      },
-    ],
-  },
-  options: {
-    legend: {
-      display: true,
-      onClick: (e) => e.stopPropagation(),
-      labels: { fontColor: "#ffffff", fontsize: 20 },
-    },
-  },
-});
+/* global Chart */
 
-var eloChartCtx = document.getElementById("eloChart").getContext("2d");
-var eloChart = new Chart(eloChartCtx, {
-  type: "line",
-  data: {
-    labels: [
-      "Jan",
-      "Feb",
-      "Mär",
-      "Apr",
-      "Mai",
-      "Jun",
-      "Jul",
-      "Aug",
-      "Sep",
-      "Okt",
-    ],
-    datasets: [
-      {
-        data: [282, 350, 411, 502, 635, 809, 947, 1402, 3700, 5267],
-        label: "Elo",
-        borderColor: "#31bebeff",
-        fill: false,
-      },
-    ],
-  },
-  options: {
-    legend: { display: false },
-    title: {
-      display: false,
-      text: "Elo over time",
-      fontColor: "#ffffff",
+window.addEventListener('load', function () {
+  const winLoosePieCtx = document.getElementById('winLoosePie').getContext('2d')
+  // eslint-disable-next-line no-unused-vars
+  const winLoosePie = new Chart(winLoosePieCtx, {
+    type: 'pie',
+    data: {
+      labels: ['Wins', 'Looses'],
+      datasets: [
+        {
+          data: [90, 20],
+          borderWidth: 0,
+          backgroundColor: ['#31bebeff', '#12141eff']
+        }
+      ]
     },
-  },
-});
+    options: {
+      legend: {
+        display: true,
+        onClick: (e) => e.stopPropagation(),
+        labels: { fontColor: '#ffffff', fontsize: 20 }
+      }
+    }
+  })
+
+  const eloChartCtx = document.getElementById('eloChart').getContext('2d')
+  // eslint-disable-next-line no-unused-vars
+  const eloChart = new Chart(eloChartCtx, {
+    type: 'line',
+    data: {
+      labels: [
+        'Jan',
+        'Feb',
+        'Mär',
+        'Apr',
+        'Mai',
+        'Jun',
+        'Jul',
+        'Aug',
+        'Sep',
+        'Okt'
+      ],
+      datasets: [
+        {
+          data: [282, 350, 411, 502, 635, 809, 947, 1402, 3700, 5267],
+          label: 'Elo',
+          borderColor: '#31bebeff',
+          fill: false
+        }
+      ]
+    },
+    options: {
+      legend: { display: false },
+      title: {
+        display: false,
+        text: 'Elo over time',
+        fontColor: '#ffffff'
+      }
+    }
+  })
+})
--- page/statistics.js
+++ page/statistics.js
@@ -1,19 +1,19 @@
 module.exports = {
-  page: page,
-};
+  page: page
+}
 
-function page(req, res) {
-  var fs = require("fs");
-  fs.readFile("html/statistics.html", "utf8", function (err, html) {
-    if (err) throw err;
+function page (req, res) {
+  const fs = require('fs')
+  fs.readFile('html/statistics.html', 'utf8', function (err, html) {
+    if (err) throw err
 
     res.end(
-      require("../src/html_creator").create_html(html, {
-        title: "Statistiken",
-        js: ["example_statistics.js"],
-        css: ["styles_general"],
-        nav: true,
+      require('../src/html_creator').create_html(html, {
+        title: 'Statistiken',
+        js: ['example_statistics'],
+        css: ['styles_general'],
+        nav: true
       })
-    );
-  });
+    )
+  })
 }
