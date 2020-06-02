92692a65f8b42a45f16bfa5af201bd4741f24450
Fabrice von der Lehr <fabrice.vonderlehr222@googlemail.com>
2020-06-02 12:19:37 +0200

    Added apiPost

--- api/enter_game.js
+++ api/enter_game.js
@@ -2,14 +2,13 @@ module.exports = {
   process: process
 }
 
-function process (playerA1, playerA2, playerB1, playerB2, goalsA, goalsB) {
-  // TODO: check if it is a 'one vs one' or a 'one vs two' or 'two vs two' game
+function process (req, playerA1, playerB1, playerA2, playerB2, goalsA, goalsB) {
+  const group = require('../src/account_util').get_group(req)
 
-  console.log('game entered')
-
-  console.log('player A1: ' + playerA1)
-  console.log('player A2: ' + playerA2)
-  console.log('player B1: ' + playerB1)
-  console.log('goals A:   ' + goalsA)
-  console.log('goals B:   ' + goalsB)
+  if (group === null) {
+    return {
+      success: false,
+      errorReason: 'no group'
+    }
+  }
 }
