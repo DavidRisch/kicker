6f044bc4afc449528aea229e7d921a4eb6f4392b
Dennis220999 <43820947+Dennis220999@users.noreply.github.com>
2020-06-02 12:09:59 +0200

    Disabled wrong switch behaviour

--- page/enter_game.js
+++ page/enter_game.js
@@ -1,16 +1,16 @@
-function page (req, res) {
-  require('fs').readFile('html/enter_game.html', 'utf8', function (err, html) {
-    if (err) throw err
+function page(req, res) {
+    require('fs').readFile('html/enter_game.html', 'utf8', function (err, html) {
+        if (err) throw err
 
-    res.end(require('../src/html_creator').create_html(html, {
-      title: 'Neues Spiel',
-      js: ['jquery', 'jquery-ui', 'tokenize2','enter_game'],
-      css: ['bootstrap', 'tokenize2', 'styles_general', 'jquery-ui'],
-      nav: true
-    }))
-  })
+        res.end(require('../src/html_creator').create_html(html, {
+            title: 'Neues Spiel',
+            js: ['jquery', 'bootstrap', 'jquery-ui', 'tokenize2', 'enter_game'],
+            css: ['bootstrap', 'tokenize2', 'styles_general', 'jquery-ui'],
+            nav: true
+        }))
+    })
 }
 
 module.exports = {
-  page: page
+    page: page
 }
