403a679c6d78954939e96f68a5384650eaed6715
Dennis220999 <43820947+Dennis220999@users.noreply.github.com>
2020-06-05 15:39:29 +0200

    detect different Players

--- js/enter_game.js
+++ js/enter_game.js
@@ -1,12 +1,4 @@
 // helper functions
-function hideElment(id, hide) {
-    const element = document.getElementById(id) // textinput A
-    if (hide) {
-        element.style.display = 'none'
-    } else {
-        element.style.display = 'inline'
-    }
-}
 
 function setCheckbox(id, checked) {
     const element = document.getElementById(id) // textinput A
@@ -21,20 +13,43 @@ function teamBHas2ndPlayer(checkbox) { // eslint-disable-line no-unused-vars
     document.getElementById('playerB2').disabled = !checkbox.checked;
 }
 
+function differentPlayers(data) { // eslint-disable-line no-unused-vars
+    if(data.playerA2 === null || data.playerB2 === null){
+        if(data.playerA1 === data.playerB1)
+            return false;
+    }
+    let playerList = [data.playerA1, data.playerA2, data.playerB1, data.playerB2];
+    const playerIDSet = new Set(playerList);
+    playerIDSet.delete(null);
+    let participantCount = 0;
+    playerList.forEach(function (item,index ) {
+        console.log(item,index);
+        if (item !== null) {
+            ++participantCount;
+        }
+    });
+    console.log(participantCount);
+    return playerIDSet.size === participantCount;
+}
 async function Submit () { // eslint-disable-line no-unused-vars
-  data = {
+    console.log('In Submit()');
+  const data = {
     action: '',
     playerA1: document.getElementById('playerA1').value,
     playerB1: document.getElementById('playerB1').value,
-    playerA2: !isADisabled ? document.getElementById('playerA2').value : null,
-    playerB2: !isBDisabled ? document.getElementById('playerB2').value : null,
+    playerA2: !document.getElementById('playerA2').disabled ? document.getElementById('playerA2').value : null,
+    playerB2: !document.getElementById('playerB2').disabled ? document.getElementById('playerB2').value : null,
     goalsA: document.getElementById('goalsAInput').value,
     goalsB: document.getElementById('goalsBInput').value
   }
+   if( differentPlayers(data)){
+       const res = await apiPost(data)
+       if (res.success) {
+           console.log('Enter game success')
+       }
+   }
+   else{
+       console.log('Chose different players')
+   }
 
-  data.action = 'enterGame'
-  const res = await apiPost(data)
-  if (res.success) {
-    console.log('Enter game success')
-  }
 }
