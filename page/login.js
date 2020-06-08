956818956871de78c40f5fb039a2660542bd2fbe
David Risch <acc_mid@rischnet.de>
2020-06-08 13:16:27 +0200

    Reformatted page/login

--- page/login.js
+++ page/login.js
@@ -5,10 +5,15 @@ module.exports = {
 function page (req, res) {
   console.log('making login page...')
 
-  var fs = require('fs')
+  const fs = require('fs')
   fs.readFile('html/login.html', 'utf8', function (err, html) {
     if (err) throw err
 
-      res.end(require('../src/html_creator').create_html(html, { title: 'Login', js: ['login', 'cookie_parser'], css: ['register_credentials', 'styles_general'], nav:false }))
+    res.end(require('../src/html_creator').create_html(html, {
+      title: 'Login',
+      js: ['login', 'cookie_parser'],
+      css: ['register_credentials', 'styles_general'],
+      nav: false
+    }))
   })
 }
